9059ecd61d9cf04359218884405a0615b180ad3b
EdsonRego <edsonxrego@gmail.com>
2022-01-05 22:56:13 -0300

    Bootstrap break point 992

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
 import Navbar from 'components/Navbar';
+
+import './styles.css';
 
 const Home = () => {
   return (
@@ -9,6 +12,8 @@ const Home = () => {
         <div className="home-card">
           <div className="home-content-container">
             <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+            <ButtonIcon />
           </div>
           <div className="home-image-container">
             <MainImage />
